57dcda2e1bd6a23bca13a80fc544ec81b33c0228
Helena Parviainen <helena.m.parviainen@cs.helsinki.fi>
2016-02-26 11:18:44 +0200

    välicommit

--- Luolastopeli/src/main/java/logic/AreaGenerator.java
+++ Luolastopeli/src/main/java/logic/AreaGenerator.java
@@ -35,42 +35,45 @@ public class AreaGenerator {
         enemies = new ArrayList<>();
         items = new ArrayList<>();
         prevCells = new boolean[width][height];
-        chanceToStartAlive = 0.45;
+        chanceToStartAlive = 0.4;
     }
 
     public void run() {
         initCells();
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 5; i++) {
             simulateStep();
+        }
+        for (int j = 0; j < 3; j++) {
+            closeIsolatedAreas();
         }
         fillWithTiles();
     }
-    
+
     public void fillWithTiles() {
         Random random = new Random();
-        
+
         for (int x = 0; x < areaWidth; x++) {
             for (int y = 0; y < areaHeight; y++) {
-                
+
                 if (nextCells[x][y]) { // cell is alive, place floor
                     tilemap[x][y] = new Tile(x, y, "FLOOR");
                     double number = random.nextDouble();
-                    
+
                     if (player == null) {
                         player = new Player(x, y);
                         tilemap[x][y].setEntity(player);
-                        
+
                     } else if (number < 0.075) {
                         Enemy enemy = new Enemy(x, y);
                         enemies.add(enemy);
                         tilemap[x][y].setEntity(enemy);
-                        
+
                     } else if (number > 0.95) {
                         Treasure treasure = new Treasure(x, y);
                         items.add(treasure);
                         tilemap[x][y].setItem(treasure);
                     }
-                    
+
                 } else { // cell is dead, place wall
                     tilemap[x][y] = new Tile(x, y, "WALL");
                 }
@@ -89,22 +92,41 @@ public class AreaGenerator {
         }
     }
 
+    public void closeIsolatedAreas() {
+        nextCells = new boolean[areaWidth][areaHeight];
+        int deathLimit = 3;
+
+        for (int x = 1; x < areaWidth - 1; x++) {
+            for (int y = 1; y < areaHeight - 1; y++) {
+                int nbs = countAliveNeighbors(x, y);
+                if (nbs <= deathLimit) {
+                    nextCells[x][y] = false;
+                } else {
+                    nextCells[x][y] = prevCells[x][y];
+                }
+            }
+        }
+
+        prevCells = nextCells;
+    }
+
     public void simulateStep() { // one step of game of life
         nextCells = new boolean[areaWidth][areaHeight];
-        int deathLimit = 2;
+        int deathLimit = 3;
         int birthLimit = 3;
-        
+
         for (int x = 1; x < areaWidth - 1; x++) {
             for (int y = 1; y < areaHeight - 1; y++) {
                 int nbs = countAliveNeighbors(x, y);
                 if (prevCells[x][y]) {
-                    if (nbs < deathLimit) {
+                    if (nbs <= deathLimit) {
+                        nextCells[x][y] = false;
+                    } else if (nbs == 8) {
                         nextCells[x][y] = false;
                     } else {
                         nextCells[x][y] = true;
                     }
-                }
-                else {
+                } else {
                     if (nbs > birthLimit) {
                         nextCells[x][y] = true;
                     } else {
@@ -114,11 +136,13 @@ public class AreaGenerator {
             }
         }
 
+        prevCells = nextCells;
+
     }
 
     public int countAliveNeighbors(int x, int y) {
         int aliveNeighbors = 0;
-        
+
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
                 int nbX = x + i;
@@ -127,7 +151,7 @@ public class AreaGenerator {
                     continue;
                 } else if (nbX < 1 || nbY < 1 || nbX >= areaWidth - 1 || nbY >= areaHeight - 1) {
                     aliveNeighbors = aliveNeighbors + 1; // count spaces outside of area as living
-                    
+
                 } else if (prevCells[nbX][nbY]) {
                     aliveNeighbors = aliveNeighbors + 1;
                 }
